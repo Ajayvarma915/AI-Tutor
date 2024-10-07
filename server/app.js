f1183630a3610fe8c1a8bb0ad3352d172685bf0b
Sarath61 <sarathsai4602@gmail.com>
2024-10-07 08:29:20 +0530

    cors updated

--- server/app.js
+++ server/app.js
@@ -6,7 +6,13 @@ const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:8000",
+  methods: "GET, POST, PUT, DELETE",
+  credential: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan("dev"));
 
