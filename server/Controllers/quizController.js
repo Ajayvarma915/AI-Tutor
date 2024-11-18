b4aa707ab5e46a1db5e89f056cd926d715a13d94
Sarath61 <sarathsai4602@gmail.com>
2024-11-18 23:30:07 +0530

    feat:Implemented API for pdf from node to python

--- server/Controllers/quizController.js
+++ server/Controllers/quizController.js
@@ -1,16 +1,54 @@
 const prisma = require("../utils/db.config");
+const axios = require("axios");
+const FormData = require("form-data");
 
 exports.StartQuizSession = async (req, res) => {
   try {
-    const { classId, userId } = req.body;
+    const { classesId, userId, coursesId } = req.body;
 
+    const sendPdf = async () => {
+      try {
+        const classPdf = await prisma.classes.findFirst({
+          where: {
+            AND: [
+              {
+                id: classesId,
+              },
+              {
+                coursesId,
+              },
+            ],
+          },
+          select: {
+            pdffile: true,
+            name: true,
+          },
+        });
+        const formData = new FormData();
+        formData.append("file", Buffer.from(classPdf.pdffile), "classes1.pdf");
+        const response = await axios.post(
+          "http://localhost:9000/pdf_generate-qa/",
+          formData,
+          {
+            headers: {
+              ...formData.getHeaders(),
+            },
+          }
+        );
+        console.log(response.data);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+    sendPdf();
     const quizSessoin = await prisma.quizSession.create({
       data: {
-        classId,
         userId,
+        classesId,
         score: 6,
       },
     });
+
     res.status(200).json({
       status: "success",
       message: "quiz created",
@@ -18,7 +56,6 @@ exports.StartQuizSession = async (req, res) => {
         quizSessoin,
       },
     });
-    
   } catch (e) {
     res.status(400).json({
       status: "Failed",
