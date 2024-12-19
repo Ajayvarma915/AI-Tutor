e7b0fbb07ea38d6e8b704ce96acb609004c75691
Sarath61 <sarathsai4602@gmail.com>
2024-12-19 18:17:03 +0530

    feat: result calculation updated

--- server/Routers/quizRouters.js
+++ server/Routers/quizRouters.js
@@ -3,7 +3,7 @@ const quizController = require("../Controllers/quizController");
 
 const router = express.Router();
 
-router.route("/generate-qa").get(quizController.StartQuizSession);
+router.route("/generate-qa").post(quizController.StartQuizSession);
 
 router.route("/ans-submission").post(quizController.answerSubmission);
 
